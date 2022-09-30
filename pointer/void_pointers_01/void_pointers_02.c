8779b7c830e0215b52d2285d566f855e2a7aa201
Evrenotur <101205887+Evrenotur@users.noreply.github.com>
2022-09-30 15:10:10 +0300

    example

---
+++ pointer/void_pointers_01/void_pointers_02.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+/*Generic Pointer  (void)
+Türden bağımsız olarak bir nesnenin adresini gösterebilen pointerlara denir.
+*/
+int main()
+{
+	int x = 20;
+	int y = 30;
+	char c;
+
+	void* ptr = &x;
+
+	printf("%d", *(int*)ptr);
+
+	return 0;
+}
---
+++ uinon/union_02/example_01.c
@@ -0,0 +1,24 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+#pragma
+#include<stdio.h>
+
+typedef union port {
+	int x, y;
+    double dval;
+    int k;
+	char str[6];
+}Ports;
+
+int main()
+{    
+	/*İlk öcenlikli değer atama ilk önce tanımlanan değişkene atanmaktadır*/
+	Ports A = {5};
+	printf("%d\n",A.x);
+	printf("%d\n", A.y);
+	printf("%lf\n", A.dval);
+    printf("%s\n", A.str);
+
+
+	return 0;
+}
